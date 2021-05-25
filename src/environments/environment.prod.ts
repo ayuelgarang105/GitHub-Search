250fa900219c053d27044dcac83f84d7eb6fbad9
Gabriel Ayuel <ayuelgarang105@gmail.com>
2021-05-26 01:58:00 +0300

    add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiKey:'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',
+  apiKey:'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',
   apiURL:'https://api.github.com/users/'
 };
